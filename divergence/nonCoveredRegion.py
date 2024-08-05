af9e83568982d4748831be6393d273f4b82049bc
Saswat KM <116586251+saswat-km@users.noreply.github.com>
2024-08-05 09:46:45 -0400

    Update nonCoveredRegion.py

--- divergence/nonCoveredRegion.py
+++ divergence/nonCoveredRegion.py
@@ -12,25 +12,29 @@ s sorang.chr1 20 7 - 203644362 GATCCTT
 
 '''
 
-mafDat = []
-with open(sys.argv[1],"r") as data:
-    for line in data:
-        dat = line.rstrip().split()
-        mafDat.append(dat[2:-2])
-
-chrSpecies = dat[1].split(".")[1]
-chrLen = int(dat[-2])
-
-mafDat = pd.DataFrame(mafDat, columns=['MAF_START','LENGTH','STRAND'])
-mafDat[["MAF_START", "LENGTH"]] = mafDat[["MAF_START", "LENGTH"]].apply(pd.to_numeric)
-mafDat["CHR"] = chrSpecies
-
 def calStart(maf_start, chrLen, strand):
+    '''
+    Calculate the start wrt positive strand
+    '''
     if strand == "-":
         start = chrLen - maf_start - 1
     elif strand == "+":
         start = maf_start
     return start
+
+mafDat = []
+#Read the custom file and store the start, length, and strand
+with open(sys.argv[1],"r") as data:
+    for line in data:
+        dat = line.rstrip().split()
+        mafDat.append(dat[2:-2])
+
+chrSpecies = dat[1].split(".")[1] #Note the chromosome of the species
+chrLen = int(dat[-2]) #Note the total chromosome length
+
+mafDat = pd.DataFrame(mafDat, columns=['MAF_START','LENGTH','STRAND']) #convert to dataframe
+mafDat[["MAF_START", "LENGTH"]] = mafDat[["MAF_START", "LENGTH"]].apply(pd.to_numeric)
+mafDat["CHR"] = chrSpecies
 
 mafDat["START"] = mafDat.apply(lambda row: calStart(row["MAF_START"], chrLen, row["STRAND"]), axis=1)
 mafDat["END"] = mafDat["START"] + mafDat["LENGTH"]
@@ -39,7 +43,7 @@ mafDat.drop(["MAF_START", "LENGTH","STRAND"], inplace=True, axis=1)
 mafDat = mafDat.to_csv(sep='\t', header=False, index=False)
 
 mafDatBed = pybedtools.BedTool(mafDat, from_string=True)
-mafDatBed = mafDatBed.sort().merge()
+mafDatBed = mafDatBed.sort().merge() #sort and merge the overlapping coordinates
 
 mafDat = mafDatBed.to_dataframe()
 mafDat["length"] = mafDat["end"] - mafDat["start"]
